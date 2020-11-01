498a1f6cf666522efe0efe52b8bc3c95f2854ef0
Lucas <lukas.zahoransky@zsbystricany.info>
2020-11-01 23:53:53 +0100

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -10,7 +10,7 @@ def send_email(target_user: str, send_to: str):
     server.login(api.get_email_from(), api.get_email_password())
     subject = f'{target_user} created a new repository!'
     repositories_link = f'https://github.com/{target_user}?tab=repositories'
-    body = f'Hey there! {target_user} created a new repository. Check it out: {repositories_link}'
+    body = f'Hey there!\n {target_user} created a new repository. Check it out: {repositories_link}'
     message = f'Subject: {subject}\n\n{body}'
     server.sendmail(api.get_email_from(), send_to, message)
     print(f'Sending e-mail to {send_to}.')
